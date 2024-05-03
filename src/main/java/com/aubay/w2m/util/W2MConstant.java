341224c932c89607b02ea6f89b8d1f7eda25d4ef
Pablo <pablonavarro.dev@gmail.com>
2024-05-03 16:43:46 +0200

    Control caché sobre servicios find, update y delete

--- src/main/java/com/aubay/w2m/util/W2MConstant.java
+++ src/main/java/com/aubay/w2m/util/W2MConstant.java
@@ -4,6 +4,8 @@ public class W2MConstant {
 	
 	public static final String DEFAULT_PAGE = "0";
 	public static final String DEFAULT_PAGE_SIZE = "5";
+	public static final String CACHE_NAVES = "naves";
+	public static final String CACHE_NAVES_KEY = "#id";
 	
 	private W2MConstant() {
 		
